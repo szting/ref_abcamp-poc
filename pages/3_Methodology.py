f223fa3f4cadf758f3c143d677cd4da16341561b
JudyT01 <168handmade@gmail.com>
2024-10-13 23:29:13 +0800

    Update 3_Methodology.py

--- pages/3_Methodology.py
+++ pages/3_Methodology.py
@@ -17,4 +17,14 @@ st.markdown('''**Detailed Flowchart Description For Hospital Ward Class Planning
 5.	Output to User: Display the final response to the user in the Streamlit app and await further interactions. Users can provide feedback on the response, which can loop back for further queries and back to 1. \n
 ''')
 
-st.image("test-streamlit-pic.jpg")
+st.image("ward-class-flowchart.jpg")
+
+st.markdown('''**Detailed Flowchart Description For MediSave Explainer:** \n
+1.	User Submits Query: User enters a query in the Streamlit app and the query is sent to the BaseCallbackHandler. \n
+2.	BaseCallbackHandler Receives Query: The handler receives and processes the incoming query and routed directly to the Researcher Agent. \n
+3.	Researcher Agent Processing: This agent analyses the query using ScrapeWebsiteTool and generates a response based on its analysis for Customer Service Agent to process.\n
+4.	Customer Service Agent Processing: This agent analyses the query using FileReadTool and prepare the output which is a well-structured response ready for display. \n
+5.	Output to User: Display the final response to the user in the Streamlit app and await further interactions. Users can provide feedback on the response, which can loop back for further queries and back to 1.
+''')
+
+st.image("medisave-flowchart.jpg")
