387163487b941b0c6c8d7665f27d58fa6bd3d14e
JudyT01 <168handmade@gmail.com>
2024-10-13 20:40:03 +0800

    Update 2_About_Us.py

--- pages/2_About_Us.py
+++ pages/2_About_Us.py
@@ -9,17 +9,15 @@ st.set_page_config(
 
 st.title("About Us Page")
 
-st.write("Use Cases for Navigating Healthcare Coverage and Protection (MediShield Life and Medisave):
-         
-        Hospital Ward Class Planning Simulator:
-        This use case will allow users to simulate various hospital ward class scenarios based on information, 
-        such as benefits, government subsidies, deductible, co-insurance and exclusions under the MediShield Life Scheme. 
-        Users can use this information to plan the hospital ward class that suits their budget and helping them make informed
-        decision if additional private insurance coverage is required.
-        
-        MediSave Explainer with Interactive Scenarios based on User Inputs:
-        Users can explore usage of MediSave savings through interactive scenarios that illustrate how it can 
-        help with their healthcare expenses. This feature will enhance understanding by allowing users to see 
+st.write("Use Cases for Navigating Healthcare Coverage and Protection (MediShield Life and Medisave): \n
+         Hospital Ward Class Planning Simulator:\n
+        This use case will allow users to simulate various hospital ward class scenarios based on information, \n
+        such as benefits, government subsidies, deductible, co-insurance and exclusions under the MediShield Life Scheme. \n
+        Users can use this information to plan the hospital ward class that suits their budget and helping them make \n
+        informed decision if additional private insurance coverage is required.\n
+        MediSave Explainer with Interactive Scenarios based on User Inputs: \n
+        Users can explore usage of MediSave savings through interactive scenarios that illustrate how it can \n
+        help with their healthcare expenses. This feature will enhance understanding by allowing users to see \n
         the practical implications of using MediSave savings in their healthcare financing.")
 
 st.write("This is a Streamlit App that demonstrates how to use the OpenAI API to generate text completions.")
