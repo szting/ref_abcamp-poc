0d9c52b02752395d00442962ccd80e3ba7b0ffd8
JudyT01 <168handmade@gmail.com>
2024-10-13 23:23:49 +0800

    Update 3_Methodology.py

--- pages/3_Methodology.py
+++ pages/3_Methodology.py
@@ -7,8 +7,14 @@ st.set_page_config(
 )
 # endregion <--------- Streamlit App Configuration --------->
 
-st.title("About this App")
+st.title("Methodology Page")
 
-st.write("This is a Streamlit App that demonstrates how to use the OpenAI API to generate text completions.")
+st.markdown('''**Detailed Flowchart Description For Hospital Ward Class Planning Simulator:** \n
+1.	User Submits Query: User enters a query in the Streamlit app and the query is sent to the BaseCallbackHandler. \n
+2.	BaseCallbackHandler Receives Query: The handler receives and processes the incoming query and routed directly to the Information Retrieval Agent. \n
+3.	Information Retrieval Agent Processing: This agent analyses the query using PDFSearchTool and generates a response based on its analysis for Customer Service Agent to process. \n
+4.	Customer Service Agent Processing: This agent analyses the query using FileReadTool and prepare the output which is a well-structured response ready for display. \n
+5.	Output to User: Display the final response to the user in the Streamlit app and await further interactions. Users can provide feedback on the response, which can loop back for further queries and back to 1. \n
+''')
 
-st.image("test-streamlit-pic.jpg", caption="Diagram flow")
+st.image("test-streamlit-pic.jpg")
