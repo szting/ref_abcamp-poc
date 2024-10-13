616d0e8c2538dd840d19e5a37750cf8818d270f7
JudyT01 <168handmade@gmail.com>
2024-10-13 20:36:41 +0800

    Update 2_About_Us.py

--- pages/2_About_Us.py
+++ pages/2_About_Us.py
@@ -7,6 +7,23 @@ st.set_page_config(
 )
 # endregion <--------- Streamlit App Configuration --------->
 
-st.title("About this App")
+st.title("About Us Page")
+
+st.write("Use Cases for Navigating Healthcare Coverage and Protection (MediShield Life and Medisave):
+         
+        Hospital Ward Class Planning Simulator:
+        This use case will allow users to simulate various hospital ward class scenarios based on information, 
+        such as benefits, government subsidies, deductible, co-insurance and exclusions under the MediShield Life Scheme. 
+        Users can use this information to plan the hospital ward class that suits their budget and helping them make informed
+        decision if additional private insurance coverage is required.
+        
+        MediSave Explainer with Interactive Scenarios based on User Inputs:
+        Users can explore usage of MediSave savings through interactive scenarios that illustrate how it can 
+        help with their healthcare expenses. This feature will enhance understanding by allowing users to see 
+        the practical implications of using MediSave savings in their healthcare financing.")
+
+st.write("This is a Streamlit App that demonstrates how to use the OpenAI API to generate text completions.")
+
+st.write("This is a Streamlit App that demonstrates how to use the OpenAI API to generate text completions.")
 
 st.write("This is a Streamlit App that demonstrates how to use the OpenAI API to generate text completions.")
