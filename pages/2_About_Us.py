50da6b06eecb55ebefc4da86c1debfca2144228d
JudyT01 <168handmade@gmail.com>
2024-10-13 21:17:08 +0800

    Update 2_About_Us.py

--- pages/2_About_Us.py
+++ pages/2_About_Us.py
@@ -11,26 +11,27 @@ st.title("About Us Page")
 
 st.markdown('''**Use Cases for Navigating Healthcare Coverage and Protection (MediShield Life and Medisave):** \n
         
-        Hospital Ward Class Planning Simulator: \n
-        This use case will allow users to simulate various hospital ward class scenarios 
-        based on information, such as benefits, government subsidies, deductible, co-insurance 
-        and exclusions under the MediShield Life Scheme. Users can use this information to plan 
-        the hospital ward class that suits their budget and helping them make informed decision 
-        if additional private insurance coverage is required.\n
+            Hospital Ward Class Planning Simulator: \n
+        This use case will allow users to simulate various hospital ward class 
+        scenarios based on information, such as benefits, government subsidies, 
+        deductible, co-insurance and exclusions under the MediShield Life Scheme. 
+        Users can use this information to plan the hospital ward class that suits 
+        their budget and helping them make informed decision if additional private 
+        insurance coverage is required.\n
         
         MediSave Explainer with Interactive Scenarios based on User Inputs: \n
-        Users can explore usage of MediSave savings through interactive scenarios that illustrate 
-        how it can help with their healthcare expenses. This feature will enhance understanding 
-        by allowing users to see the practical implications of using MediSave savings in their 
-        healthcare financing.''')
+        Users can explore usage of MediSave savings through interactive scenarios 
+        that illustrate how it can help with their healthcare expenses. This 
+        feature will enhance understanding by allowing users to see the practical
+        implications of using MediSave savings in their healthcare financing.''')
 
 st.markdown('''**Objectives:** \n
 
-•	To simplify the process of finding and comparing hospital ward class options.
-•	To provide clear and concise information about healthcare financing features and benefits.
-•	To assist users in selecting the best hospital ward class plan based on their personal requirements and financial constraints.
-•	To offer guidance on usage of MediSave savings in their healthcare financing.
-•	Provide 24/7 access to information, making it easier for users to understand their options healthcare financing needs.
+•	To simplify the process of finding and comparing hospital ward class options.\n
+•	To provide clear and concise information about healthcare financing features and benefits.\n
+•	To assist users in selecting the best hospital ward class plan based on their personal requirements and financial constraints.\n
+•	To offer guidance on usage of MediSave savings in their healthcare financing.\n
+•	Provide 24/7 access to information, making it easier for users to understand their options healthcare financing needs.\n
 •	Decrease the workload on customer service representatives by automating responses to common queries.
 ''')
 
